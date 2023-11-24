6f7205ec3aff0bb28f21a0f5c8ca42376e35e07a
Tomas Neubauer <tomas@quix.io>
2023-11-24 14:03:15 +0000

    Edited 'main.py'

--- AI Customer/main.py
+++ AI Customer/main.py
@@ -14,7 +14,6 @@ from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseries
 
 app = Application.Quix("transformation-v1", auto_offset_reset="latest")
 
-input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
 output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
 
@@ -114,7 +113,7 @@ def get_answer(row: dict):
     #publish_rp(custreply)
     print("I have sent my reply to the agent.")
 
-sdf = app.dataframe(input_topic)
+sdf = app.dataframe(output_topic)
 def call_llm(row: dict, callback):
 
     result = llm(row["chat-message"])
