f830a7340de41a73fbd15fa663f857bf2b82824a
Tomas Neubauer <tomas@quix.io>
2023-11-24 19:30:49 +0000

    Edited 'main.py'

--- AI Customer/main.py
+++ AI Customer/main.py
@@ -68,7 +68,6 @@ def generate_response(row, prompt, max_tokens=250, temperature=0.7, top_p=0.95, 
         iteration_text = iteration["choices"][0]["text"]
         response += iteration_text
         row["chat-message"] = response
-        row["draft"] = True
         draft_producer.produce(row, message_key())
         print(str(iteration_text), end="", flush=True)
 
