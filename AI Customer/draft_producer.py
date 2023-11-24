63eb0779c4bae11671f3d5cd39189320c1e01a0e
Tomas Neubauer <tomas@quix.io>
2023-11-24 14:52:33 +0000

    Edited 'draft_producer.py'

--- AI Customer/draft_producer.py
+++ AI Customer/draft_producer.py
@@ -4,9 +4,9 @@ import json
 
 class DraftProducer:
 
-    def __init__(self):
-         cfg_builder = QuixKafkaConfigsBuilder()
-        cfgs, topics, _ = cfg_builder.get_confluent_client_configs([os.environ["output"]])
+    def __init__(self, topic: str):
+        cfg_builder = QuixKafkaConfigsBuilder()
+        cfgs, topics, _ = cfg_builder.get_confluent_client_configs([])
         self.topic = topics[0]
 
         self.producer = Producer(cfgs.pop("bootstrap.servers"), extra_config=cfgs)
