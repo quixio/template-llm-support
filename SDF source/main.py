b42cc135ee1eb47c91bda475c4d0ef7e0eb1b6f7
Tomas Neubauer <tomas@quix.io>
2023-11-24 10:33:12 +0000

    Edited 'main.py'

--- SDF source/main.py
+++ SDF source/main.py
@@ -1,20 +1,59 @@
+from quixstreams.kafka import Producer
+from quixstreams.platforms.quix import QuixKafkaConfigsBuilder
 import os
-from quixstreams import Application, State
-from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
+import random
+import timedelta
+import pandas as pd
+import datetime
+import json
 
 
-app = Application.Quix("transformation-v1", auto_offset_reset="latest")
+def main():
 
-input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
-output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
-sdf = app.dataframe(input_topic)
+    cfg_builder = QuixKafkaConfigsBuilder()
+    cfgs, topics, _ = cfg_builder.get_confluent_client_configs([os.environ["output"]])
+    topic = topics[0]
 
-# Here put transformation logic.
+    with Producer(
+        cfgs.pop("bootstrap.servers"), extra_config=cfgs
+    ) as producer:
+        
+        # Creating a sample DataFrame
 
-sdf.apply(lambda row, ctx: print(row))
+        # Sample data parameters
+        num_samples = 10  # Number of samples to generate
+        user_ids = [1001, 1002, 1003, 1004, 1005]  # Example user IDs
+        impressions = ['click', 'view', 'interact']  # Example impression types
 
-sdf.to_topic(output_topic)
+        # Generate sample data
+        data = {
+            'Timestamp': [datetime.now() - timedelta(days=random.randint(0, 30)) for _ in range(num_samples)],
+            'user_id': [random.choice(user_ids) for _ in range(num_samples)],
+            'impression': [random.choice(impressions) for _ in range(num_samples)]
+        }
+
+        # Create DataFrame
+        df = pd.DataFrame(data)
+
+        # Displaying the first few rows of the DataFrame
+        print(df)
+
+        for row in df.iterrows():
+
+ 
+            producer.produce(
+                topic=topic,
+                key=row["user_id"],
+                value=json.dumps(row),
+            )
+
+            print(dict(row))
+
+
 
 if __name__ == "__main__":
-    app.run(sdf)
+    main()
+
+
+
