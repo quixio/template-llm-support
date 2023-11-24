64f3c2be94e2f48fc37115d5cfacb21eed2d3538
Sadeep Madurange <sadeep@quix.ai>
2023-11-24 16:57:57 +0800

    Use timeseries data.

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -1,19 +1,20 @@
 import os, sys
 import quixstreams as qx
-import pandas as pd
 from pathlib import Path
 from llama_cpp import Llama
 from datetime import datetime
 from huggingface_hub import hf_hub_download
 
+AGENT_ROLE = "agent"
+CUSTOMER_ROLE = "customer"
+
 role = os.environ["role"].lower()
-if role != "agent" and role != "customer":
+if role != AGENT_ROLE and role != CUSTOMER_ROLE:
     print("Error: invalid role {}".format(role))
     sys.exit(1)
 
 model_name = "llama-2-7b-chat.Q4_K_M.gguf"
 model_path = "./state/{}".format(model_name)
-
 if not Path(model_path).exists():
     print("The model path does not exist in state. Downloading model...")
     hf_hub_download("TheBloke/Llama-2-7b-Chat-GGUF", model_name, local_dir="state")
@@ -28,34 +29,37 @@ topic_consumer = client.get_topic_consumer(os.environ["topic"])
 
 product = os.environ["product"]
 scenario = """
-            The following transcript represents a converstation between a 
-            customer support agent who works for a large electronics retailer called 
-            'ACME electronics', and a customer who has bought a defective {} and 
-            wants to understand what their options are for resolving the issue. 
+            The following transcript represents a converstation between a customer 
+            support agent who works for a large electronics retailer called 'ACME 
+            electronics', and a customer who has bought a defective {} and wants to 
+            understand what their options are for resolving the issue. 
             Please continue the conversation, but only reply as {}:
             """.format(product, role.upper())
 
 def on_stream_recv_handler(sc: qx.StreamConsumer):
     print("Received stream {}".format(sc.stream_id))
 
-    def on_data_recv_handler(_: qx.StreamConsumer, df: pd.DataFrame):
-        sender = df["role"][0].upper()
+    def on_data_recv_handler(_: qx.StreamConsumer, data: qx.TimeseriesData):
+        ts = data.timestamps[0]
+        sender = ts.parameters["role"].string_value
         if sender != role:
-            msg = df["text"][0]
+            msg = ts.parameters["text"].string_value
             print("{}: {}".format(sender, msg))
             reply = ""
             print("{}: {}".format(role, reply))
             
-            data = {
-                "timestamp": [datetime.utcnow()],
-                "role": [role], 
-                "text": [reply], 
-                "conversation_id": ["002"]
-            }
-            sp = topic_producer.get_or_create_stream(sc.stream_id)
-            sp.timeseries.buffer.publish(pd.DataFrame(data))
+            td = qx.TimeseriesData()
+            td.add_timestamp(datetime.utcnow()) \
+              .add_value("role", role) \
+              .add_value("text", reply) \
+              .add_value("conversation_id", "002")
 
-    sc.timeseries.on_dataframe_received = on_data_recv_handler
+            sp = topic_producer.get_or_create_stream(sc.stream_id)
+            sp.timeseries.buffer.publish(td)
+
+    buf = sc.timeseries.create_buffer()
+    buf.packet_size = 1
+    buf.on_data_released = on_data_recv_handler
 
 topic_consumer.on_stream_received = on_stream_recv_handler
 
