df499b74ce94eebe063c248af179f3e832f24a4a
Tomas Neubauer <tomas@quix.io>
2023-11-27 14:41:08 +0000

    Edited 'main.py'

--- SentimentAnalysis/main.py
+++ SentimentAnalysis/main.py
@@ -5,7 +5,7 @@ from transformers import pipeline
 
 classifier = pipeline('sentiment-analysis')
 
-app = Application.Quix("sentiment-analysis-v1", auto_offset_reset="earliest")
+app = Application.Quix("sentiment-analysis-v2", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
 #output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
