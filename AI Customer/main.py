e6d683ec78c0b82b34d8f339b209143010b3f5bc
Tomas Neubauer <tomas@quix.io>
2023-11-27 14:55:51 +0000

    Edited 'main.py'

--- AI Customer/main.py
+++ AI Customer/main.py
@@ -63,7 +63,7 @@ def get_answer(row: dict, state: State):
 
 
 sdf = sdf[sdf["Tags"].contains("name")]
-sdf = sdf[sdf["Tags"]["name"] != role]
+sdf = sdf[sdf["Tags"]["name"] == role]
 
 
 sdf = sdf.apply(get_answer, stateful=True)
