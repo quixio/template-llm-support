c3e2495a64333e3a2b4ca8fff2ee03dbe80b051e
Tomas Neubauer <tomas@quix.io>
2023-11-27 16:04:58 +0000

    Edited 'main.py'

--- Converstations sink/main.py
+++ Converstations sink/main.py
@@ -2,19 +2,15 @@ import os
 from quixstreams import Application, State
 from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
 
+app = Application.Quix("transformation-v10-customer", auto_offset_reset="latest")
 
-app = Application.Quix("transformation-v1", auto_offset_reset="latest")
 
 input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
-output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
 sdf = app.dataframe(input_topic)
 
-# Here put transformation logic.
-
 sdf = sdf.update(lambda row: print(row))
 
-sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
