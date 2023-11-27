5b47585efbf5b54e5e5150e8572183c16f4e8517
Tomas Neubauer <tomas@quix.io>
2023-11-27 14:52:03 +0000

    Edited 'main.py'

--- SentimentAnalysis/main.py
+++ SentimentAnalysis/main.py
@@ -8,17 +8,8 @@ classifier = pipeline('sentiment-analysis')
 app = Application.Quix("sentiment-analysis-v4", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
-#output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
+output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
-
-
-                # Calculate "sentiment" feature using label for sign and score for magnitude
-                #df.loc[i, "sentiment"] = row["score"] if row["label"] == "POSITIVE" else - row["score"]
-
-                # Add average sentiment (and update memory)
-                #self.count = self.count + 1
-                #self.sum = self.sum + df.loc[i, "sentiment"]
-                #df.loc[i, "average_sentiment"] = self.sum/self.count
 
 storage_key = "mean_v1"
 
@@ -34,20 +25,17 @@ def mean(row: dict, state: State):
 
     return sum(mean_state) / len(mean_state)
 
+
+# Pipeline definition.
 sdf = app.dataframe(input_topic)
 
 sdf["sentiment"] = sdf["chat-message"].apply(lambda value: classifier(value)[0])
 sdf["sentiment"] = sdf.apply(lambda row: float(row["sentiment"]["score"]) if row["sentiment"]["label"] == "POSITIVE" else -float(row["sentiment"]["score"]))
 
-
-
 sdf["average_sentiment"] = sdf.apply(mean, stateful=True)
-
-# Here put transformation logic.
 
 sdf = sdf.update(lambda row: print(row))
 
-#sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
