6a12c5149e5b24d2971927c69cc6f98f50775fd2
Tomas Neubauer <tomas@quix.io>
2023-11-27 20:37:15 +0000

    Edited 'main.py'

--- ConversationStarter/main.py
+++ ConversationStarter/main.py
@@ -1,6 +1,4 @@
 import os
-from quixstreams import Application, State
-from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
 from quixstreams.models.serializers import (
     QuixTimeseriesSerializer,
     SerializationContext,
@@ -10,13 +8,9 @@ from quixstreams.platforms.quix import QuixKafkaConfigsBuilder
 import uuid
 import time
 
-app = Application.Quix("transformation-v1", auto_offset_reset="latest")
-
-input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
-output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
 cfg_builder = QuixKafkaConfigsBuilder()
-cfgs, topics, _ = cfg_builder.get_confluent_client_configs([topic])
+cfgs, topics, _ = cfg_builder.get_confluent_client_configs([os.environ["output"]])
 topic = topics[0]
 
 producer = Producer(cfgs.pop("bootstrap.servers"), extra_config=cfgs)
@@ -34,7 +28,7 @@ row = {
   "NumericValues": {},
   "StringValues": {
     "chat-message": [
-      " Thank you so much for sharing your experience with us, {customer}. I can understand why it would be frustrating when dealing with customer support representatives who are not helpful or knowledgeable about the product they're supporting. Unfortunately, we have had issues in the past where our representatives may not always have all the necessary information to address a problem right away, but please know that their priority is still"
+      "Hello, welcome to ACME Electronics support, my name is Percy. How can I help you today?"
     ]
   },
   "TagValues": {
