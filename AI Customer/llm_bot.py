5ff1b10eab2e72ab59071ba15b8b9eb76f593d16
Tomas Neubauer <tomas@quix.io>
2023-11-27 11:39:54 +0000

    Edited 'llm_bot.py'

--- AI Customer/llm_bot.py
+++ AI Customer/llm_bot.py
@@ -25,8 +25,6 @@ class LlmBot:
 
     
     def generate_response(self, row, prompt, max_tokens=250, temperature=0.7, top_p=0.95, repeat_penalty=1.2, top_k=150):
-        draft_producer.produce(row, message_key())
-        
         result = self.llm(
             prompt=prompt,
             max_tokens=max_tokens,
