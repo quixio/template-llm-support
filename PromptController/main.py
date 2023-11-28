092b2038eebcd4370933b76d822f3f22b9b0c5dc
Tomas Neubauer <tomas@quix.io>
2023-11-28 16:31:42 +0000

    Edited 'main.py'

--- PromptController/main.py
+++ PromptController/main.py
@@ -8,20 +8,32 @@ app = Application.Quix("prompt-controller-v1", auto_offset_reset="earliest")
 input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
 output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
+state_key = "state-v1"
+
 sdf = app.dataframe(input_topic)
 sdf = sdf[sdf["Tags"]["name"] != "director"]
 
 sdf = sdf[sdf.contains("average_sentiment_count")]
 
-def update_row(row: dict):
+def update_row(row: dict, state: State):
     row['Timestamp']: time.time_ns()
     row['Tags']['name'] = "director"
 
-sdf = sdf.update(update_row)
+    state_value = state.get(state_key, 0)
+
+    if state_value > 0:
+        print("Already sent")
+        return None
+    else:
+        state.set(state_key, 1)
+        return row
+
 
 
 sdf = sdf[sdf["average_sentiment"] < -0.5 and sdf["average_sentiment_count"] > 3]
 sdf["chat-message"] = "The sentiment of this conversation is very negative, can you increase politeness?"
+
+sdf = sdf.apply(update_row, stateful=True)
 
 sdf = sdf.update(lambda row: print(row))
 
