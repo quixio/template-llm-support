464602c23692766c839f92176c2c219ac37980b2
David Levy <david@quix.ai>
2023-11-28 10:15:13 +0100

    Remove text

--- Sentiment Demo UI/src/app/app.component.ts
+++ Sentiment Demo UI/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { QuixService } from './services/quix.service';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +6,4 @@ import { QuixService } from './services/quix.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent{
-
-  workspaceId: string;
-  ungatedToken: string;
-
-  constructor(private quixService: QuixService) {
-    this.workspaceId = this.quixService.workspaceId;
-    this.ungatedToken = this.quixService.ungatedToken;
-  }
 }
