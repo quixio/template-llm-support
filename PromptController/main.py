8039d7b4b6941eb52f537a35adf8235929e0e4ea
Tomas Neubauer <tomas@quix.io>
2023-11-28 11:29:04 +0000

    Edited 'main.py'

--- PromptController/main.py
+++ PromptController/main.py
@@ -1,7 +1,7 @@
 import os
 from quixstreams import Application, State
 from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
-
+import time
 
 app = Application.Quix("prompt-controller-v1", auto_offset_reset="earliest")
 
@@ -13,12 +13,14 @@ sdf = app.dataframe(input_topic)
 
 sdf = sdf[sdf.contains("average_sentiment_count")]
 
+sdf["Tags"] = sdf["Tags"].apply(lambda: row["role"] = "director")
+
+sdf = sdf.update(lambda value: value['Timestamp'] = time.time_ns())
 
 sdf = sdf.update(lambda row: print(f"average_sentiment: {row['average_sentiment']}, count: {row['average_sentiment_count']}"))
 
 
 sdf = sdf[sdf["average_sentiment"] < -0.5 and sdf["average_sentiment_count"] > 3]
-
 
 
 
