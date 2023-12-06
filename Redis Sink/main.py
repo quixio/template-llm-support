9769c7ed985a2f0fc11b44c2682328562efb7bb6
Sadeep Madurange <sadeep+demo@quix.io>
2023-12-06 08:10:45 +0000

    Save to redis.

--- Redis Sink/main.py
+++ Redis Sink/main.py
@@ -1,8 +1,8 @@
 import quixstreams as qx
 import os
-import pandas as pd
 
 import redis
+from redis.commands.json.path import Path
 
 r = redis.Redis(
   host=os.environ["redis_host"],
@@ -14,8 +14,22 @@ client = qx.QuixStreamingClient()
 topic_consumer = client.get_topic_consumer(topic_id_or_name = os.environ["input"])
 
 def on_stream_recv_handler(sc: qx.StreamConsumer):
-    def on_data_recv_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-        print(df)
+    
+    def on_data_recv_handler(stream_consumer: qx.StreamConsumer, data: qx.TimeseriesData):
+        for ts in data:
+            entry = {
+                "text": ts.parameters["text"].string_value,
+                "role": ts.parameters["role"].string_value,
+                "sentiment": ts.parameters["sentiment"].numeric_value,
+                "conversation_id": ts.parameters["conversation_id"].string_value
+            }
+
+            cached = r.json().get(sc.stream_id)
+            if not cached:
+                cached = []
+                
+            cached.append(entry)
+            r.json().set(sc.stream_id, Path.root_path(), cached)
     
     sc.timeseries.on_dataframe_received = on_data_recv_handler
 
