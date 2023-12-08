f7067867be0a2692361bf1f75a900722768ba6f5
Sadeep Madurange <sadeep+demo@quix.io>
2023-12-08 05:49:07 +0000

    Edited 'draft_producer.py'

---
+++ AI Customer/draft_producer.py
@@ -0,0 +1,32 @@
+from quixstreams.kafka import Producer
+from quixstreams.platforms.quix import QuixKafkaConfigsBuilder
+from quixstreams.models.serializers import (
+    QuixTimeseriesSerializer,
+    SerializationContext,
+)
+import uuid
+
+class DraftProducer:
+
+    def __init__(self, topic: str):
+        cfg_builder = QuixKafkaConfigsBuilder()
+        cfgs, topics, _ = cfg_builder.get_confluent_client_configs([topic])
+        self.topic = topics[0]
+
+        self.producer = Producer(cfgs.pop("bootstrap.servers"), extra_config=cfgs)
+        self.serialize = QuixTimeseriesSerializer()
+
+    def __del__(self):
+        self.producer.flush()
+            
+    def produce(self, row: dict, key: str):
+
+        headers = {**self.serialize.extra_headers, "uuid": str(uuid.uuid4())}
+
+        self.producer.produce(
+            headers=headers,
+            topic=self.topic,
+            key=key,
+            value=self.serialize(
+                value=row, ctx=SerializationContext(topic=self.topic, headers=headers)
+            ))
