15ea13fa9077ac2d22d28c6859c9f77af709caae
Sadeep Madurange <sadeep+demo@quix.io>
2023-12-11 10:08:54 +0000

    Page favicon.

--- Streamlit Dashboard/main.py
+++ Streamlit Dashboard/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="LLM Customer Support",
-    page_icon="favicon.png",
+    page_icon="favicon.ico",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
