8b175ef7b3112415ddacda57329d58e7352f460f
Sadeep Madurange <16175248+smadurange@users.noreply.github.com>
2023-12-11 18:26:12 +0800

    Use streaming data frames in the LLM agent and customer (#10)
    * Update deployment Redis Sink
    
    * Update deployment AI Customer
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Customer
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Customer
    
    * Update deployment AI Support Agent
    
    * Deleted application files from openblas_simple
    
    * Deleted application files from open_llama
    
    * Deleted application files from Llama2 Container
    
    * Use SDF.
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Quixstreams version.
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Quix streams version.
    
    * wip: SDF.
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Use SDF.
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Log received message.
    
    * Update deployment AI Customer
    
    * Log input message.
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Support Agent
    
    * Edited 'main.py'
    
    * Initialization code.
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Created 'draft_producer'
    
    * Rename 'draft_producer.py'
    
    * Edited 'draft_producer.py'
    
    * Edited 'main.py'
    
    * Edited 'main.py'
    
    * Delete 'draft_producer.py'
    
    * Restore agent code with SDF.
    
    * Initialize chat using producer.
    
    * Rename local variable.
    
    * Producer topic.
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Add topic test-topic
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Customer
    
    * Update deployment AI Support Agent
    
    * Delete topic test-topic
    
    * Delete topic default
    
    * Update deployment AI Customer
    
    * Update deployment AI Customer
    
    * Update deployment AI Support Agent
    
    * Update deployment AI Support Agent
    
    * Update deployment Redis Sink
    
    * Update deployment Redis Sink
    
    ---------
    
    Co-authored-by: Sadeep Madurange <sadeep+demo@quix.io>
    Co-authored-by: Quix Git Service <@quix>

--- AI Customer Support Agent/main.py
+++ AI Customer Support Agent/main.py
@@ -1,10 +1,13 @@
 import os
+import time
 import uuid
 import random
 from pathlib import Path
-from datetime import datetime
 
-import quixstreams as qx
+from quixstreams import Application
+from quixstreams.kafka import Producer
+from quixstreams.platforms.quix import QuixKafkaConfigsBuilder, TopicCreationConfigs
+from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer, SerializationContext
 
 from huggingface_hub import hf_hub_download
 
@@ -14,14 +17,12 @@ from langchain.chains import ConversationChain
 from langchain_experimental.chat_models import Llama2Chat
 from langchain.memory import ConversationTokenBufferMemory
 
-CHAT_ID = "002"
 AGENT_ROLE = "agent"
-
 role = AGENT_ROLE
-chat_id = str(uuid.uuid4())
 
 model_name = "llama-2-7b-chat.Q4_K_M.gguf"
 model_path = "./state/{}".format(model_name)
+
 if not Path(model_path).exists():
     print("The model path does not exist in state. Downloading model...")
     hf_hub_download("TheBloke/Llama-2-7b-Chat-GGUF", model_name, local_dir="state")
@@ -51,72 +52,78 @@ memory = ConversationTokenBufferMemory(
 
 chain = ConversationChain(llm=model, prompt=load_prompt("prompt.yaml"), memory=memory)
 
-client = qx.QuixStreamingClient()
-topic_producer = client.get_topic_producer(os.environ["topic"])
-topic_consumer = client.get_topic_consumer(os.environ["topic"])
+app = Application.Quix("transformation-v10-"+role, auto_offset_reset="latest")
+input_topic = app.topic(os.environ["topic"], value_deserializer=QuixDeserializer())
+output_topic = app.topic(os.environ["topic"], value_serializer=QuixTimeseriesSerializer())
+
+cfg_builder = QuixKafkaConfigsBuilder()
+cfgs, topics, _ = cfg_builder.get_confluent_client_configs([os.environ["topic"]])
+cfg_builder.create_topics([TopicCreationConfigs(name=topics[0])])
+serializer = QuixTimeseriesSerializer()
+
+sdf = app.dataframe(topic=input_topic)
 
 def agents_init():
-    agents = []
+    out = []
 
     with open("agents.txt", "r") as fd:
         for a in fd:
             if a:
-                agents.append(a.strip())
-    return agents
+                out.append(a.strip())
+    return out
 
 agents = agents_init()
 
 def chat_init():
+    chat_id = str(uuid.uuid4())
     agent = random.choice(agents)
     greet = """Hello, welcome to ACME Electronics support, my name is {}. 
                How can I help you today?""".format(agent)
-               
-    msg = qx.TimeseriesData()
-    msg.add_timestamp(datetime.utcnow()) \
-       .add_value("role", role) \
-       .add_value("text", greet) \
-       .add_value("conversation_id", chat_id)
+    
+    headers = {**serializer.extra_headers, "uuid": chat_id}
+    value = {
+        "role": role,
+        "text": greet,
+        "conversation_id": chat_id,
+        "Timestamp": time.time_ns(),
+    }
 
-    sp = topic_producer.get_or_create_stream(chat_id)
-    sp.timeseries.publish(msg)
-
-def on_stream_recv_handler(sc: qx.StreamConsumer):
-    print("Received stream {}".format(sc.stream_id))
-
-    def on_data_recv_handler(_: qx.StreamConsumer, data: qx.TimeseriesData):
-        ts = data.timestamps[0]
-        sender = ts.parameters["role"].string_value
-        if sender != role:
-            msg = ts.parameters["text"].string_value
-            print("{}: {}\n".format(sender.upper(), msg))
-
-            if "good bye" in msg.lower():
-                print("Initializing a new conversation...")
-                memory.clear()
-                chat_init()
-                return
-
-            print("Generating response...")
-            
-            reply = chain.run(msg)
-            print("{}: {}\n".format(role.upper(), reply))
-            
-            td = qx.TimeseriesData()
-            td.add_timestamp(datetime.utcnow()) \
-              .add_value("role", role) \
-              .add_value("text", reply) \
-              .add_value("conversation_id", ts.parameters["conversation_id"].string_value)
-
-            sp = topic_producer.get_or_create_stream(sc.stream_id)
-            sp.timeseries.publish(td)
-
-    buf = sc.timeseries.create_buffer()
-    buf.packet_size = 1
-    buf.on_data_released = on_data_recv_handler
-
-topic_consumer.on_stream_received = on_stream_recv_handler
+    with Producer(broker_address=cfgs.pop("bootstrap.servers"), extra_config=cfgs) as producer:
+        producer.produce(
+            topic=topics[0],
+            headers=headers,
+            key=str(uuid.uuid4()),
+            value=serializer(value=value, ctx=SerializationContext(topic=topics[0], headers=headers)),
+        )
+    
+    print("Started chat")
 
 chat_init()
 
-print("Listening to streams. Press CTRL-C to exit.")
-qx.App.run()
+def reply(row: dict):
+    print("Replying to: {}".format(row["text"]))
+    
+    if "good bye" in row["text"].lower():
+        print("Initializing a new conversation...")
+        memory.clear()
+        chat_init()
+        return
+
+    print("Generating response...")
+    msg = chain.run(row["text"])
+    print("{}: {}\n".format(role.upper(), msg))
+    
+    row["role"] = role
+    row["text"] = msg
+    return row
+
+sdf = sdf[sdf["role"] != role]
+sdf = sdf.apply(reply, stateful=False)
+sdf = sdf[sdf.apply(lambda row: row is not None)]
+
+sdf["Timestamp"] = sdf["Timestamp"].apply(lambda row: time.time_ns())
+
+sdf = sdf.to_topic(output_topic)
+
+if __name__ == "__main__":
+    app.run(sdf)
