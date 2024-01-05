e02247470bef682adc90b4d551f1f97886c1080b
Steve Rosam <srosam+quix-demos@gmail.com>
2024-01-05 14:41:49 +0000

    Edited 'main.py'

--- fake convos/main.py
+++ fake convos/main.py
@@ -14,21 +14,13 @@ topic_producer = client.get_topic_producer(topic_id_or_name = os.environ["output
 
 # Set stream ID or leave parameters empty to get stream ID generated.
 stream = topic_producer.create_stream()
-stream.properties.name = "Hello World Python stream"
 
-# Add metadata about time series data you are about to send. 
-stream.timeseries.add_definition("ParameterA").set_range(-1.2, 1.2)
-stream.timeseries.buffer.time_span_in_milliseconds = 100
-
-print("Sending values for 30 seconds.")
-
-for index in range(0, 3000):
-    stream.timeseries \
-        .buffer \
-        .add_timestamp(datetime.datetime.utcnow()) \
-        .add_value("ParameterA", math.sin(index / 200.0) + math.sin(index) / 5.0) \
-        .publish()
-    time.sleep(0.01)
+stream.timeseries \
+    .buffer \
+    .add_timestamp(datetime.datetime.utcnow()) \
+    .add_value("text", "hi i'm Steve") \
+    .add_value("conversation_id", "abc123") \
+    .publish()
 
 print("Closing stream")
 stream.close()
