02ae06d6e3ca398581789c1912bd3e8eb85cc1dd
Steve Rosam <srosam+quix-demos@gmail.com>
2024-01-05 14:16:51 +0000

    Edited 'main.py'

--- AI Customer Support Agent/main.py
+++ AI Customer Support Agent/main.py
@@ -60,8 +60,7 @@ model = Llama2Chat(
 
 model = Llama2Chat(llm=llm,
                 system_message=SystemMessage(
-                    content=system_message_content)
-)
+                    content=system_message_content))
 
 # Defines how much of the conversation history to give to the model
 # during each exchange (300 tokens, or a little over 300 words)
