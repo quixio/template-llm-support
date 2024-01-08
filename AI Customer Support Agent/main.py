fc80f14eb0ddfe7d546f11a45c6d19c1df4f7f21
Steve Rosam <srosam+quix-demos@gmail.com>
2024-01-08 13:46:19 +0000

    Edited 'main.py'

--- AI Customer Support Agent/main.py
+++ AI Customer Support Agent/main.py
@@ -73,7 +73,7 @@ memory = ConversationTokenBufferMemory(
 # i.e "You are a support agent and need to answer the customer...".
 chain = ConversationChain(llm=model, prompt=load_prompt("prompt.yaml"), memory=memory)
 
-print(f"Prompt={chain.prompt}")
+print(f"Prompt={chain.prompt}, \nParameters={prompt.parameters}")
 
 
 # Initializes a Quix Kafka consumer with a consumer group based on the role
