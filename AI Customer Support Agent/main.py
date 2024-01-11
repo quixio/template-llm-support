d810dc6c6d5cadab1aa3bd2d8c03a65d443659d0
Steve Rosam <srosam+quix-demos@gmail.com>
2024-01-11 15:21:05 +0000

    Edited 'main.py'

--- AI Customer Support Agent/main.py
+++ AI Customer Support Agent/main.py
@@ -274,7 +274,8 @@ def reply(row: dict, state: State):
     # containing the agents role and reply 
     return row
 
-# sdf = sdf.apply(lambda row: print(row))
+sdf = sdf.update(lambda row: print("GOT THIS NEW ROW!"))
+sdf = sdf.update(lambda row: print(row))
 
 # Filter the SDF to include only incoming rows where the roles that dont match the bot's current role
 # So that it doesn't reply to its own messages
