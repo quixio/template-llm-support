12bd080bc0df82f0ee305e474babd4f5872e0048
Steve Rosam <srosam+quix-demos@gmail.com>
2024-01-11 20:47:37 +0000

    Edited 'main.py'

--- AI Customer Support Agent/main.py
+++ AI Customer Support Agent/main.py
@@ -88,7 +88,7 @@ model = Llama2Chat(
 
 # Initializes a Quix Kafka consumer with a consumer group based on the role
 # and configured to read the latest message if no offset was previously registered for the consumer group
-app = Application.Quix("transformation-v17-"+role, auto_offset_reset="latest")
+app = Application.Quix("transformation-v17-"+role, auto_offset_reset="earliest")
 
 # Defines the input and output topics with the relevant deserialization and serialization methods (and get the topic names from enviroiment variables)
 
