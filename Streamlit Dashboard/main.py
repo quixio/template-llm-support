4a10b89e342332cb7a15df7b8f80ae59c117e69a
Merlin Carter <merlin+demo@quix.io>
2024-01-19 08:01:07 +0000

    Edited 'main.py'

--- Streamlit Dashboard/main.py
+++ Streamlit Dashboard/main.py
@@ -6,7 +6,7 @@ import time
 import altair as alt
 import datetime
 
-os.environ["GRPC_DEFAULT_SSL_ROOTS_FILE_PATH"] = r"./cacert.pem"
+os.environ["GRPC_DEFAULT_SSL_ROOTS_FILE_PATH"] = "./cacert.pem"
 measurement_name = "conversations"
 
 # Initialize the client variable
